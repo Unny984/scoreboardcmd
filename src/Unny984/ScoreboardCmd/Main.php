7cfb0017486f7040041d41c308eeabc13f657d8a
Unny984 <sunny72158@gmail.com>
2025-01-15 03:03:14 +0800

    a

--- src/Unny984/ScoreboardCmd/Main.php
+++ src/Unny984/ScoreboardCmd/Main.php
@@ -1,117 +1,29 @@
 <?php
 
-namespace Unny984\ScoreboardCmd;
+namespace Unny984\ScoreCountdown;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use pocketmine\scheduler\TaskHandler;
-use pocketmine\utils\TextFormat;
-use pocketmine\world\World;
+use Unny984\ScoreCountdown\TimerAddon;
 
 class Main extends PluginBase {
 
-    private ?TaskHandler $countdownTaskHandler = null;
+    private ?TimerAddon $timerAddon = null;
 
     public function onEnable(): void {
-        $this->getLogger()->info(TextFormat::GREEN . "ScoreboardCmd enabled!");
+        $this->timerAddon = new TimerAddon($this);
+        $this->getLogger()->info("ScoreCountdown enabled!");
     }
 
-    public function onDisable(): void {
-        $this->getLogger()->info(TextFormat::RED . "ScoreboardCmd disabled!");
-        if ($this->countdownTaskHandler !== null) {
-            $this->countdownTaskHandler->cancel();
+    public function startCountdown(Player $player, int $time): void {
+        if ($this->timerAddon !== null) {
+            $this->timerAddon->startTimer($player, $time);
         }
     }
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        if (!$sender instanceof Player) {
-            $sender->sendMessage(TextFormat::RED . "This command can only be used in-game.");
-            return true;
+    public function stopCountdown(Player $player): void {
+        if ($this->timerAddon !== null) {
+            $this->timerAddon->stopTimer($player);
         }
-
-        switch ($command->getName()) {
-            case "countdown":
-                if (count($args) !== 1 || !is_numeric($args[0])) {
-                    $sender->sendMessage(TextFormat::YELLOW . "Usage: /countdown <time_in_seconds>");
-                    return true;
-                }
-
-                $time = (int)$args[0];
-                if ($time <= 0) {
-                    $sender->sendMessage(TextFormat::RED . "Please specify a positive number of seconds.");
-                    return true;
-                }
-
-                $this->startCountdown($sender, $time);
-                return true;
-
-            case "stopcountdown":
-                $this->stopCountdown($sender);
-                return true;
-        }
-
-        return false;
-    }
-
-    private function startCountdown(Player $player, int $time): void {
-        if ($this->countdownTaskHandler !== null) {
-            $player->sendMessage(TextFormat::RED . "A countdown is already running.");
-            return;
-        }
-
-        $player->sendMessage(TextFormat::GREEN . "Countdown started for {$time} seconds.");
-
-        $task = new class($player, $time, $this) extends \pocketmine\scheduler\Task {
-            private Player $player;
-            private int $remainingTime;
-            private Main $plugin;
-
-            public function __construct(Player $player, int $time, Main $plugin) {
-                $this->player = $player;
-                $this->remainingTime = $time;
-                $this->plugin = $plugin;
-            }
-
-            public function onRun(): void {
-                if ($this->remainingTime <= 0) {
-                    $this->plugin->stopCountdown($this->player);
-                    $this->player->sendMessage(TextFormat::GREEN . "Countdown finished!");
-                    return;
-                }
-
-                $minutes = intdiv($this->remainingTime, 60);
-                $seconds = $this->remainingTime % 60;
-                $title = sprintf("%02d:%02d", $minutes, $seconds);
-                $this->plugin->updateScoreboard($this->player, $title);
-
-                $this->remainingTime--;
-            }
-        };
-
-        $this->countdownTaskHandler = $this->getScheduler()->scheduleRepeatingTask($task, 20); // Runs every second
-    }
-
-    private function stopCountdown(Player $player): void {
-        if ($this->countdownTaskHandler === null) {
-            $player->sendMessage(TextFormat::RED . "No countdown is currently running.");
-            return;
-        }
-
-        $this->countdownTaskHandler->cancel();
-        $this->countdownTaskHandler = null;
-        $this->removeScoreboard($player);
-        $player->sendMessage(TextFormat::GREEN . "Countdown stopped.");
-    }
-
-    public function updateScoreboard(Player $player, string $title): void {
-        // Send a scoreboard popup to the player
-        $player->sendPopup(TextFormat::GOLD . "Countdown Timer\n" . TextFormat::YELLOW . $title);
-    }
-
-    public function removeScoreboard(Player $player): void {
-        // Clear the scoreboard popup
-        $player->sendPopup("");
     }
 }
---
+++ src/Unny984/ScoreboardCmd/TimerAddon.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Unny984\ScoreCountdown;
+
+use Ifera\ScoreHud\event\TagsResolveEvent;
+use Ifera\ScoreHud\ScoreHud;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
+use pocketmine\scheduler\Task;
+
+class TimerAddon implements Listener {
+
+    private PluginBase $plugin;
+    private array $timers = [];
+
+    public function __construct(PluginBase $plugin) {
+        $this->plugin = $plugin;
+        $this->plugin->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function startTimer(Player $player, int $time): void {
+        $this->timers[$player->getName()] = $time;
+        $this->plugin->getScheduler()->scheduleRepeatingTask(new class($this, $player) extends Task {
+            private TimerAddon $addon;
+            private Player $player;
+
+            public function __construct(TimerAddon $addon, Player $player) {
+                $this->addon = $addon;
+                $this->player = $player;
+            }
+
+            public function onRun(): void {
+                $name = $this->player->getName();
+                if (!isset($this->addon->timers[$name])) {
+                    return;
+                }
+
+                $time = $this->addon->timers[$name]--;
+                if ($time <= 0) {
+                    $this->addon->stopTimer($this->player);
+                }
+            }
+        }, 20);
+    }
+
+    public function stopTimer(Player $player): void {
+        unset($this->timers[$player->getName()]);
+    }
+
+    /**
+     * Handle ScoreHud placeholders
+     */
+    public function onTagsResolve(TagsResolveEvent $event): void {
+        $player = $event->getPlayer();
+        $name = $player->getName();
+
+        if (isset($this->timers[$name])) {
+            $time = $this->timers[$name];
+            $minutes = intdiv($time, 60);
+            $seconds = $time % 60;
+            $event->getTags()->setTag("scorecountdown.timer", sprintf("%02d:%02d", $minutes, $seconds));
+        }
+    }
+}
