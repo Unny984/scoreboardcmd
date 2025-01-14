ae013f47eb58943a1e73d72ffc71cecd3f1d7555
Unny984 <sunny72158@gmail.com>
2025-01-15 04:18:07 +0800

    Update TimerAddon.php

--- src/Unny984/ScoreboardCmd/TimerAddon.php
+++ src/Unny984/ScoreboardCmd/TimerAddon.php
@@ -3,7 +3,6 @@
 namespace Unny984\ScoreboardCmd;
 
 use Ifera\ScoreHud\event\TagsResolveEvent;
-use Ifera\ScoreHud\scoreboard\ScoreTag;
 use pocketmine\event\Listener;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
@@ -11,7 +10,7 @@ use pocketmine\plugin\PluginBase;
 class TimerAddon implements Listener {
 
     private PluginBase $plugin;
-    private array $timers = [];
+    private ?int $timer = null;
 
     public function __construct(PluginBase $plugin) {
         $this->plugin = $plugin;
@@ -19,7 +18,7 @@ class TimerAddon implements Listener {
         // Register this class as an event listener
         $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
 
-        // Schedule a repeating task to update timers
+        // Schedule a repeating task to update the timer
         $plugin->getScheduler()->scheduleRepeatingTask(new class($this) extends \pocketmine\scheduler\Task {
             private TimerAddon $addon;
 
@@ -28,55 +27,59 @@ class TimerAddon implements Listener {
             }
 
             public function onRun(): void {
-                $this->addon->updateTimers();
+                $this->addon->updateTimer();
             }
         }, 20); // Runs every second
     }
 
-    public function setTimer(Player $player, int $time): void {
-        $this->plugin->getLogger()->info("Setting timer for player: {$player->getName()} with time: {$time}");
-        $this->timers[$player->getName()] = $time;
+    public function setTimer(int $time): void {
+        $this->plugin->getLogger()->info("Setting global timer with time: {$time}");
+        $this->timer = $time;
     }
 
-    public function getTimer(Player $player): ?int {
-        return $this->timers[$player->getName()] ?? null;
-    }
-
-    public function clearTimer(Player $player): void {
-        $this->plugin->getLogger()->info("Clearing timer for player: {$player->getName()}");
-        unset($this->timers[$player->getName()]);
+    public function clearTimer(): void {
+        $this->plugin->getLogger()->info("Clearing global timer");
+        $this->timer = null;
     }
 
     public function updateTimers(): void {
-        foreach ($this->timers as $name => $time) {
-            if ($time > 0) {
-                $this->plugin->getLogger()->info("Updating timer for player: {$name} to " . ($time - 1));
-                $this->timers[$name]--;
-            } else {
-                $this->plugin->getLogger()->info("Timer for player: {$name} has ended.");
-                unset($this->timers[$name]);
+    foreach ($this->timers as $name => $time) {
+        if ($time > 0) {
+            $this->plugin->getLogger()->info("Updating timer for player: {$name} to " . ($time - 1));
+            $this->timers[$name]--;
+
+            // Trigger TagsResolveEvent to update the timer dynamically
+            $player = $this->plugin->getServer()->getPlayerExact($name);
+            if ($player !== null) {
+                $event = new TagsResolveEvent($player);
+                $event->call();
             }
+        } else {
+            $this->plugin->getLogger()->info("Timer for player: {$name} has ended.");
+            unset($this->timers[$name]);
         }
     }
+}
+
 
     public function onTagsResolve(TagsResolveEvent $event): void {
         $player = $event->getPlayer();
-        $name = $player->getName();
-    
-        if (isset($this->timers[$name])) {
-            $time = $this->timers[$name];
-            $minutes = intdiv($time, 60);
-            $seconds = $time % 60;
-    
-            $this->plugin->getLogger()->info("Setting timer in TagsResolveEvent: {$minutes}:{$seconds}");
-    
-            // Set the placeholder using ScoreTag
-            $event->setTag(new ScoreTag("scorecountdown.timer", sprintf("%02d:%02d", $minutes, $seconds)));
+
+        if ($this->timer !== null) {
+            $minutes = intdiv($this->timer, 60);
+            $seconds = $this->timer % 60;
+
+            $this->plugin->getLogger()->info("Setting global timer in TagsResolveEvent: {$minutes}:{$seconds}");
+
+            // Set the placeholder to the global timer value
+            $event->setTag([
+                "scorecountdown.timer" => sprintf("%02d:%02d", $minutes, $seconds)
+            ]);
         } else {
-            $this->plugin->getLogger()->info("No active timer for player in TagsResolveEvent: $name");
-    
-            // Set the placeholder to a default value
-            $event->setTag(new ScoreTag("scorecountdown.timer", "00:00"));
+            $this->plugin->getLogger()->info("No active global timer in TagsResolveEvent.");
+            $event->setTag([
+                "scorecountdown.timer" => "00:00"
+            ]);
         }
     }
 }
