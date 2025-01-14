1444b6770cc72cddb060f91ba21807644fafe393
Unny984 <sunny72158@gmail.com>
2025-01-15 03:29:23 +0800

    Update Main.php

--- src/Unny984/ScoreboardCmd/Main.php
+++ src/Unny984/ScoreboardCmd/Main.php
@@ -3,6 +3,8 @@
 namespace Unny984\ScoreboardCmd;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 
 class Main extends PluginBase {
@@ -12,6 +14,39 @@ class Main extends PluginBase {
     public function onEnable(): void {
         $this->timerAddon = new TimerAddon($this);
         $this->getLogger()->info("ScoreboardCmd has been enabled!");
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("This command can only be used in-game.");
+            return true;
+        }
+
+        switch ($command->getName()) {
+            case "countdown":
+                // Ensure there is exactly one argument and it is numeric
+                if (count($args) !== 1 || !ctype_digit($args[0])) {
+                    $sender->sendMessage("Usage: /countdown <time_in_seconds>");
+                    return false;
+                }
+
+                $time = (int)$args[0];
+                if ($time <= 0) {
+                    $sender->sendMessage("Please specify a positive number of seconds.");
+                    return false;
+                }
+
+                $this->startCountdown($sender, $time);
+                $sender->sendMessage("Countdown started for {$time} seconds.");
+                return true;
+
+            case "stopcountdown":
+                $this->stopCountdown($sender);
+                $sender->sendMessage("Countdown stopped.");
+                return true;
+        }
+
+        return false;
     }
 
     public function startCountdown(Player $player, int $time): void {
