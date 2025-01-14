d3a3c9071346dbfaeb50d6bfefd7a826d4448eca
Unny984 <sunny72158@gmail.com>
2025-01-15 03:45:18 +0800

    Update TimerAddon.php

--- src/Unny984/ScoreboardCmd/TimerAddon.php
+++ src/Unny984/ScoreboardCmd/TimerAddon.php
@@ -16,23 +16,24 @@ class TimerAddon implements Listener {
 
     public function __construct(PluginBase $plugin) {
         $this->plugin = $plugin;
-
+    
         // Register this class as an event listener
         $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
-
+    
         // Schedule a repeating task to update the timers
         $plugin->getScheduler()->scheduleRepeatingTask(new class($this) extends Task {
             private TimerAddon $addon;
-
+    
             public function __construct(TimerAddon $addon) {
                 $this->addon = $addon;
             }
-
+    
             public function onRun(): void {
                 $this->addon->updateTimers();
             }
         }, 20); // Runs every second
     }
+    
 
     public function setTimer(Player $player, int $time): void {
         $this->timers[$player->getName()] = $time;
@@ -59,17 +60,21 @@ class TimerAddon implements Listener {
     public function onTagsResolve(TagsResolveEvent $event): void {
         $player = $event->getPlayer();
         $name = $player->getName();
-
+    
+        $this->plugin->getLogger()->info("TagsResolveEvent triggered for player: $name");
+    
         if (isset($this->timers[$name])) {
             $time = $this->timers[$name];
             $minutes = intdiv($time, 60);
             $seconds = $time % 60;
-
+    
+            $this->plugin->getLogger()->info("Setting timer: {$minutes}:{$seconds}");
+    
             // Create a ScoreTag and set it
             $event->setTag(new ScoreTag("scorecountdown.timer", sprintf("%02d:%02d", $minutes, $seconds)));
         } else {
-            // Default value when the timer is not set
+            $this->plugin->getLogger()->info("No active timer for player: $name");
             $event->setTag(new ScoreTag("scorecountdown.timer", "00:00"));
         }
-    }
+    }    
 }
