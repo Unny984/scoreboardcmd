8eef3bdd63675a794a224c8c3f9fdd59e2c4f80e
Unny984 <sunny72158@gmail.com>
2025-01-15 17:21:42 +0800

    gdfs

--- src/Unny984/ScoreboardCmd/Main.php
+++ src/Unny984/ScoreboardCmd/Main.php
@@ -2,101 +2,48 @@
 
 namespace Unny984\ScoreboardCmd;
 
+use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use pocketmine\plugin\PluginBase;
-use Ifera\ScoreHud\event\TagsResolveEvent;
 
 class Main extends PluginBase {
-    private array $timers = [];
 
-    protected function onEnable(): void {
-        $this->getServer()->getPluginManager()->registerEvents(new ScoreHudListener($this), $this);
+    private TimerAddon $timerAddon;
 
-        $this->getScheduler()->scheduleRepeatingTask(new class($this) extends \pocketmine\scheduler\Task {
-            private Main $plugin;
-
-            public function __construct(Main $plugin) {
-                $this->plugin = $plugin;
-            }
-
-            public function onRun(): void {
-                $this->plugin->updateTimers();
-            }
-        }, 20); // Runs every second
+    public function onEnable(): void {
+        $this->timerAddon = new TimerAddon($this);
     }
-
-    public function setTimer(Player $player, int $time): void {
-        $this->timers[$player->getName()] = $time;
-        $player->sendMessage("Countdown started for {$time} seconds!");
-    }
-
-    public function clearTimer(Player $player): void {
-        unset($this->timers[$player->getName()]);
-        $player->sendMessage("Your countdown has been stopped.");
-    }
-
-    public function getTimer(Player $player): ?int {
-        return $this->timers[$player->getName()] ?? null;
-    }
-
-    public function updateTimers(): void {
-        foreach ($this->timers as $name => $time) {
-            if ($time > 0) {
-                $this->timers[$name]--;
-    
-                $player = $this->getServer()->getPlayerExact($name);
-                if ($player !== null && $player->isOnline()) {
-                    // Format the timer value as MM:SS
-                    $minutes = intdiv($this->timers[$name], 60);
-                    $seconds = $this->timers[$name] % 60;
-                    $formattedTime = sprintf("%02d:%02d", $minutes, $seconds);
-    
-                    // Dynamically resolve the timer tag using TagsResolveEvent
-                    $event = new \Ifera\ScoreHud\event\TagsResolveEvent(
-                        $player,
-                        [new \Ifera\ScoreHud\scoreboard\ScoreTag("scorecountdown.timer", $formattedTime)]
-                    );
-                    $event->call(); // Correctly call the event
-                }
-            } else {
-                unset($this->timers[$name]);
-            }
-        }
-    }
-    
-    
-    
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         if (!$sender instanceof Player) {
             $sender->sendMessage("This command can only be used in-game.");
-            return false;
+            return true;
         }
 
         switch ($command->getName()) {
             case "countdown":
                 if (isset($args[0]) && is_numeric($args[0])) {
                     $time = (int)$args[0];
-                    if ($time > 0) {
-                        $this->setTimer($sender, $time);
-                        return true;
-                    } else {
-                        $sender->sendMessage("Please provide a positive number for the countdown.");
-                        return false;
-                    }
+                    $this->timerAddon->setTimer($sender, $time);
+                    $sender->sendMessage("Countdown started for {$time} seconds.");
                 } else {
                     $sender->sendMessage("Usage: /countdown <time_in_seconds>");
-                    return false;
                 }
-
-            case "stopcountdown":
-                $this->clearTimer($sender);
                 return true;
 
-            default:
-                return false;
+            case "stopcountdown":
+                $this->timerAddon->clearTimer($sender);
+                $sender->sendMessage("Countdown stopped.");
+                return true;
+
+            case "debugscorehud":
+                $time = $this->timerAddon->getTimer($sender);
+                $timeMessage = $time !== null ? "{$time} seconds remaining." : "No active timer.";
+                $sender->sendMessage("Debug info: $timeMessage");
+                return true;
         }
+
+        return false;
     }
 }
