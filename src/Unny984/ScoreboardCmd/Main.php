abb9dcbbba2535d284cb218facced564c20a4938
Unny984 <sunny72158@gmail.com>
2025-01-15 17:04:17 +0800

    Update Main.php

--- src/Unny984/ScoreboardCmd/Main.php
+++ src/Unny984/ScoreboardCmd/Main.php
@@ -6,6 +6,7 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
+use Ifera\ScoreHud\event\TagsResolveEvent;
 
 class Main extends PluginBase {
     private array $timers = [];
